f972b977ddc23ee593d47cc5b17019a917440e4c
Clare Tomlinson <ctomlinson01@qub.ac.uk>
2022-07-15 16:41:10 +0100

    re-worked event info page structure

--- cep-project/src/Event.js
+++ cep-project/src/Event.js
@@ -1,40 +1,30 @@
 import React from "react";
 import './Event.css';
-import { Card, Button, Nav } from "react-bootstrap";
+import { Card, Tab, Tabs } from "react-bootstrap";
 
 export const Event = () => (
-    <Card>
-      <Card.Header>
-        <Nav variant="tabs" defaultActiveKey="#first">
-          <Nav.Item>
-            <Nav.Link href="#first" className="event-nav">Event Information</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="#link" className="event-nav">Link</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="#" className="event-nav">
-              Disabled
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
-      </Card.Header>
-      <Card.Body>
-        <Card.Title>Event Name</Card.Title>
-        <Card.Text className="event-text">
-          <p>With supporting text below as a natural lead-in to additional content.</p>
-          <p>With supporting text below as a natural lead-in to additional content.</p>
-          <p>With supporting text below as a natural lead-in to additional content.</p>
-          <p>With supporting text below as a natural lead-in to additional content.</p>
-          <p>With supporting text below as a natural lead-in to additional content.</p>
-          <p>With supporting text below as a natural lead-in to additional content.</p>
-          <p>With supporting text below as a natural lead-in to additional content.</p>
-          <p>With supporting text below as a natural lead-in to additional content.</p>
-          <p>With supporting text below as a natural lead-in to additional content.</p>
-          <p>With supporting text below as a natural lead-in to additional content.</p>
-          <p>With supporting text below as a natural lead-in to additional content.</p>
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+
+<Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3 event-tabs">
+    <Tab eventKey="event-info" title="Event Information">
+        <card>
+            <div className="event-p">1</div>
+        </card>
+    </Tab>
+    <Tab eventKey="location" title="Location">
+        <card>
+            <div className="event-p">2</div>
+        </card>
+    </Tab>
+    <Tab eventKey="buy-tickets" title="Buy Tickets">
+        <card>
+            <div className="event-p">3</div>
+        </card>
+    </Tab>
+    <Tab eventKey="comments" title="Comments">
+        <card>
+            <div className="event-p">3</div>
+        </card>
+    </Tab>
+</Tabs>
+
 )
