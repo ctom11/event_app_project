157e5f20c07cc728f97d0d4a77af2a622dcd5fdc
Clare Tomlinson <ctomlinson01@qub.ac.uk>
2022-07-31 22:55:39 +0100

    session creation working

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -2,7 +2,10 @@ const { application } = require('express');
 const express = require('express');
 const db = require("../config/db");
 const router = express.Router();
+// for comparing input password with hashed password in db
 const bcrypt = require("bcrypt");
+// sign function creates the token
+const {sign} =  require("jsonwebtoken");
 
 router.post("/", async (req, res)=> {
 
@@ -11,16 +14,22 @@ router.post("/", async (req, res)=> {
     const attemptedPassword = req.body.password
     console.log(emailAddress);
     try {
-        const [rows, fields] = await db.promiseDb.query("SELECT password FROM user_account WHERE email_address = ?",[emailAddress]);
+        const [rows, fields] = await db.promiseDb.query("SELECT user_account_id, password FROM user_account WHERE email_address = ?",[emailAddress]);
         if(rows.length <= 0){
-            res.send("Incorrect email")
+            res.send({error: "Incorrect email"})
+            return;
         }
+        let accountId = rows[0].user_account_id
         let password = rows[0].password;
         bcrypt.compare(attemptedPassword, password).then((match) => {
             if(!match){
                 res.send({error: "Wrong Username and password combination"})
+                return;
             }
-            res.send("You logged in")
+
+            const accessToken = sign({email_address: emailAddress, user_account_id: accountId}, "q1p0w2o9e3i8r4u7t5y6");
+            res.json(accessToken);
+            return;
         })
     } catch(err) {
         throw err;
