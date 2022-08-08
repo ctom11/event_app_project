0d5ad069efb091af45f5877b46374e4af0ff82dd
Clare Tomlinson <ctomlinson01@qub.ac.uk>
2022-08-08 12:30:21 +0100

    logout working

--- cep-project/src/App.js
+++ cep-project/src/App.js
@@ -10,29 +10,49 @@ import { NavigationBar } from './components/NavigationBar';
 import { Event } from './pages/event/Event';
 import { Userprofile } from './pages/userprofile/Userprofile';
 import { CreateEvent } from './pages/event/CreateEvent';
+import { AuthContext } from './components/AuthContext'; 
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
-class App extends Component {
-  render() {
+function App() {
+
+    const[authState, setAuthState] = useState({firstName: "", id: 0, adminStatus: 0});
+
+    useEffect(() => {
+      Axios.get('http://localhost:3001/login/auth', { headers: {
+        accessToken: localStorage.getItem('accessToken')
+      }}).then((Response) => {
+        if (Response.data.error) {
+          setAuthState(false);
+        } else {
+          setAuthState(true);
+        }
+      })
+    }, []);
+
+
     return (
       /*react fragment allows you to group multiple elements*/
       <React.Fragment>
         <NavigationBar/>
         <Layout>
-          <Router>
-            <Routes>
-              <Route exact path ='/' element = {<Home/>}/>
-              <Route path ='/whatson' element = {<Whatson/>}/>
-              <Route path ='/signup' element = {<Signup/>}/>
-              <Route path ='/login' element = {<Login/>}/>
-              <Route path ='/event/:id' element = {<Event/>}/>
-              <Route path ='/userprofile/:id' element = {<Userprofile/>}/>
-              <Route path ='/createEvent' element = {<CreateEvent/>}/>
-            </Routes>
-          </Router>
+          <AuthContext.Provider value={{ authState, setAuthState }}>
+            <Router>
+              <Routes>
+                <Route exact path ='/' element = {<Home/>}/>
+                <Route path ='/whatson' element = {<Whatson/>}/>
+                <Route path ='/signup' element = {<Signup/>}/>
+                <Route path ='/login' element = {<Login/>}/>
+                <Route path ='/event/:id' element = {<Event/>}/>
+                <Route path ='/userprofile/:id' element = {<Userprofile/>}/>
+                <Route path ='/createEvent' element = {<CreateEvent/>}/>
+              </Routes>
+            </Router>
+          </AuthContext.Provider>
         </Layout>
       </React.Fragment>
     );
   }
-}
+
 
 export default App;
