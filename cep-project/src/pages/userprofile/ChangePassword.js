d1031a8a302f764df23fdab79e61fe9bf886d6e2
Clare Tomlinson <ctomlinson01@qub.ac.uk>
2022-08-19 18:03:28 +0100

    update password with formik not working

--- cep-project/src/pages/userprofile/ChangePassword.js
+++ cep-project/src/pages/userprofile/ChangePassword.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import { Card } from "react-bootstrap";
 import Axios from 'axios';
 import './Userprofile.css';
@@ -8,12 +10,23 @@ export const ChangePassword = () => {
 
     let navigate = useNavigate()
 
-    const [updatedPassword, setUpdatedPassword] = useState('')
+    /*have to create initial values for Formik*/
+    const initialValues = {
+        updatedPassword: "",
+        updatedPasswordReenter: ""
+    }
+
+    /*provides validation on form input*/
+    const validationSchema = Yup.object().shape({  
+    updatedPassword: Yup.string().min(8).max(30).required().label("Password"), //Event time must be a string and is required
+    updatedPasswordReenter: Yup.string().required().label("Password").oneOf([Yup.ref('password'), null], 'Passwords must match'),//Event location must be a string and is required
+    })
+
+
     let { id } = useParams();
 
-    const changePassword = () => {
-        Axios.post(`http://localhost:3001/useraccount/changepassword/${id}`, {
-            updatedPassword: updatedPassword},
+    const changePassword = (data) => {
+        Axios.post(`http://localhost:3001/useraccount/changepassword/${id}`, data,
             {
                 headers: {
                     accessToken: localStorage.getItem("accessToken"),
@@ -34,18 +47,23 @@ export const ChangePassword = () => {
 
         <Card className="change-name-div">
             <h1 className="change-title-password">Change Your Password</h1>
+
+            <Formik initialValues={initialValues} onSubmit={changePassword} validationSchema={validationSchema}>
+                <Form>
             <div className="mb-3">
-                <label htmlFor="inputcurrentpassword" className="form-label change-label">New Password</label>
-                <input type="password" className="form-control login change-input" id="inputcurrentpassword"
-                onChange={(e) => {
-                    setUpdatedPassword(e.target.value);
-                  }}></input>
+                <label className="form-label change-label">New Password</label><br/>
+                <ErrorMessage name="updatedPassword" component="span"/>
+                <Field type="password" className="form-control login change-input" name="updatedPassword" id="updatedPassword"></Field>
             </div>
             <div className="mb-3">
-                <label htmlFor="inputreenterpassword" className="form-label change-label">Re-enter Password:</label>
-                <input type="password" className="form-control login change-input" id="inputreenterpassword"></input>
+                <label className="form-label change-label">Re-enter Password:</label><br/>
+                <ErrorMessage name="updatedPasswordReenter" component="span"/>
+                <Field type="password" className="form-control login change-input" name="updatedPasswordReenter" id="updatedPasswordReenter"></Field>
             </div>
             <button type="submit" className="btn btn-primary eventure-btn change-btn" onClick={changePassword}>Save</button>
+            </Form>
+            </Formik>
+
         </Card> 
     )
 
